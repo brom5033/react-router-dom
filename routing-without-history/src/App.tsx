e1b085ffe82326e046db05bbeb312a538e9c6ee2
brom5033 <sjajdcjddlsl@naver.com>
2024-01-04 17:49:15 +0900

    feat(history , app): add event

--- routing-without-history/src/App.tsx
+++ routing-without-history/src/App.tsx
@@ -1,17 +1,31 @@
-import React, { MouseEvent } from "react";
+import React, { MouseEvent, useEffect, useState } from "react";
 
 function App() {
-  const pathname = window.location.pathname;
+  const [pathname, setPathname] = useState(window.location.pathname);
 
   function handleGoHome() {
     window.history.pushState(null, "", "/");
+    setPathname("/");
   }
 
   function handleGoAbout(event: MouseEvent<HTMLAnchorElement>) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute("href");
+    if (href === null) return;
     window.history.pushState(null, "", href);
+    setPathname(href);
   }
+
+  useEffect(() => {
+    const callback = () => {
+      setPathname(window.location.pathname);
+    };
+
+    window.addEventListener("popstate", callback);
+    return () => {
+      window.removeEventListener("popstate", callback);
+    };
+  }, []);
 
   return (
     <div>
