add2a39e53a83c10c894838fb152e23a878df943
brom5033 <sjajdcjddlsl@naver.com>
2024-01-04 11:30:10 +0900

    feat(app): add router url

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,22 @@ function App() {
   return (
     <div>
       <h1>Basic Example</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/dashboard">DashBoard</a>
+          </li>
+          <li>
+            <a href="/nothing-here">Nothing Here</a>
+          </li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<>Home</>} />
         <Route path="about" element={<>About</>} />
