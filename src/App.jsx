d7f8f2b8ca5b46cb7bb0dffc940b1e836c3205bf
brom5033 <sjajdcjddlsl@naver.com>
2024-01-04 11:56:45 +0900

    feat(authlayout): add element link

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./routes/Layout";
+import AuthLayout from "./routes/AuthLayout";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<>Home</>} />
-          <Route path="about" element={<>About</>} />
-          <Route path="dashboard" element={<>Dashboard</>} />
-          <Route element={<></>}>
-            <Route path="login" element={<>Login</>} />
-            <Route path="logout" element={<>Logout</>} />
-          </Route>
-          <Route path="*" element={<>Not Found</>} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<>Home</>} />
+        <Route path="about" element={<>About</>} />
+        <Route path="dashboard" element={<>Dashboard</>} />
+        <Route element={<AuthLayout />}>
+          <Route path="login" element={<>Login</>} />
+          <Route path="logout" element={<>Logout</>} />
         </Route>
-      </Routes>
+        <Route path="*" element={<>Not Found</>} />
+      </Route>
+    </Routes>
   );
 }
 
