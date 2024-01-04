dd639e003deb28234cd713dbe629cd517c114504
brom5033 <sjajdcjddlsl@naver.com>
2024-01-04 11:51:32 +0900

    chore(layout): move link tag to layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,8 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import Layout from "./routes/Layout";
 
 function App() {
   return (
-    <div>
-      <h1>Basic Example</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">DashBoard</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route element={<>Home</>} />
@@ -29,7 +11,6 @@ function App() {
           <Route path="*" element={<>Not Found</>} />
         </Route>
       </Routes>
-    </div>
   );
 }
 
--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,7 +1,26 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div><Outlet /></div>
-  )
+    <div>
+      <h1>Basic Example</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">DashBoard</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
