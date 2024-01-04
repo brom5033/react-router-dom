4b6424d578331a4d23a59aa12e9b72ce8036ad8c
brom5033 <sjajdcjddlsl@naver.com>
2024-01-04 14:00:40 +0900

    feat(team): add usesearchparams , add click function

--- src/routes/Team.jsx
+++ src/routes/Team.jsx
@@ -1,10 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useCallback } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 
 export default function Team() {
   const { teamId } = useParams();
+  const [searchParams, setSearchPrams] = useSearchParams();
+  const message = searchParams.get("message");
+
+  const handleClick = useCallback(() => {
+    setSearchPrams({
+      message: "world",
+    });
+  }, [setSearchPrams]);
+
   return (
     <div>
       <h3>Team:{teamId}</h3>
+      <button onClick={handleClick}>Click</button>
+      {message && <h4>{message}</h4>}
     </div>
   );
 }
